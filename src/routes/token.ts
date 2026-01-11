46b3d1bc38f6a5153e2000665e4704d28216f444
HOLIKOV Oleksandr <oleksandr.holikov@epitech.eu>
2026-01-11 22:56:01 +0100

    feat: create middlewares for email befor creating token

--- src/routes/token.ts
+++ src/routes/token.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
+import formatEmail from "../middlewares/formaters/email.js";
+import validEmail from "../middlewares/validators/email.js";
 
 const router = express.Router();
 const tokens = new Map<string, string>();
 
-router.post("/", (req, res)=>{
+router.post("/", validEmail, formatEmail,(req, res)=>{
     const {email} = req.body;
 
     const token = uuidv4();
