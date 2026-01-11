3e5518d88cb945f28f134291ff6d80b066a115eb
HOLIKOV Oleksandr <oleksandr.holikov@epitech.eu>
2026-01-11 22:31:18 +0100

    feat: create function for create token with email

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import justifyRouter from "./routes/justify.js"
+import tokenRouter from "./routes/token.js"
+
 
 const PORT = 3000;
 const app = express();
@@ -7,5 +9,6 @@ app.use(express.json());
 app.use(express.text({type: 'text/plain'}));
 
 app.use("/api/justify", justifyRouter)
+app.use("/api/token", tokenRouter)
 
 app.listen(PORT, ()=>console.log(`Listening on port ${PORT}`))
