1bb4cbc36477b3acdff00d40a8e90718c734975f
HOLIKOV Oleksandr <oleksandr.holikov@epitech.eu>
2026-01-11 22:13:40 +0100

    feat: creat justify function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import justifyRouter from "./routes/justify.js"
 
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 app.use(express.text({type: 'text/plain'}));
+
+app.use("/api/justify", justifyRouter)
 
 app.listen(PORT, ()=>console.log(`Listening on port ${PORT}`))
