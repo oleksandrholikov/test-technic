e212e845f8735afb5b1c7d9eb45273e3780efbac
HOLIKOV Oleksandr <oleksandr.holikov@epitech.eu>
2026-01-12 16:06:46 +0100

    fix: move check limit function to middleware

--- src/routes/justify.ts
+++ src/routes/justify.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { justifyText } from "../utils/justifyText.js";
-import { checkCharLimit } from "../utils/charLimiter.js";
 import formatText from "../middlewares/formaters/text.js";
 import validText from "../middlewares/validators/text.js";
-import { authToken } from "../middlewares/auth/token.js";
+import authToken from "../middlewares/auth/token.js";
+import charLimit from "../middlewares/limits/charLimit.js";
+
 
 const router = express.Router();
 
@@ -11,13 +12,10 @@ router.post("/",
     validText,
     formatText,
     authToken,
+    charLimit,
     (req, res)=>{
 
-    const text = req.body;
-    if(!text || typeof text !== "string") return res.status(404).send("No text provided!");
-
-    const wordsCount = text.split(/\s+/).filter(Boolean).length;
-    if(!checkCharLimit(token, wordsCount)) return res.status(402).send("Payment Required")
+    const text = req.body;    
 
     const justifiedText = justifyText(text);
     res.type("text/plain").send(justifiedText) 
