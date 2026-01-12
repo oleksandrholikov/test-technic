d0702d85854c2ed803a6aca87a251e883034f6ab
HOLIKOV Oleksandr <oleksandr.holikov@epitech.eu>
2026-01-12 15:51:12 +0100

    fix: move checkToken to middleware

---
+++ src/middlewares/auth/token.ts
@@ -0,0 +1,22 @@
+import type { Request, Response, NextFunction } from "express";
+import { tokens } from "../../routes/token.js";
+
+export function authToken(
+    req: Request,
+    res: Response,
+    next: NextFunction
+){
+    const auth = req.headers.authorization;
+
+    if(!auth)
+        return res.status(401).send("Missing token!");
+
+    const token = auth.replace("Bearer ", "").trim();
+
+    if(!tokens.has(token)) {
+        return res.status(403).send("Invalid token!");        
+    }
+
+    (req as any).token = token;
+    next();
+}
--- src/routes/justify.ts
+++ src/routes/justify.ts
@@ -3,21 +3,15 @@ import { justifyText } from "../utils/justifyText.js";
 import { checkCharLimit } from "../utils/charLimiter.js";
 import formatText from "../middlewares/formaters/text.js";
 import validText from "../middlewares/validators/text.js";
-import { tokens } from "../routes/token.js";
-
+import { authToken } from "../middlewares/auth/token.js";
 
 const router = express.Router();
 
 router.post("/", 
     validText,
     formatText,
+    authToken,
     (req, res)=>{
-        
-    const auth = req.headers.authorization;
-    if(!auth) return res.status(401).send("Missing token!");
-
-    const token = auth.replace("Bearer ", "").trim();
-    if(!tokens.has(token)) return res.status(403).send("Invalid token!");
 
     const text = req.body;
     if(!text || typeof text !== "string") return res.status(404).send("No text provided!");
